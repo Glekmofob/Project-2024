19ce7dca26bfd13dba2f3b3b2d82db82856b2102
Shanechka <161927448+Glekmofob@users.noreply.github.com>
2024-05-27 23:49:53 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+const fs = require('fs'); // Для чтения файла
+
+const app = express();
+app.use(cors());  
+
+// Обработка запроса к GeoJSON
+app.get('/api/moskva', (req, res) => {
+  fs.readFile('nenetskii-avtonomnyi-okrug.geojson', 'utf8', (err, data) => {
+    if (err) {
+      console.error('Ошибка чтения файла:', err);
+      res.status(500).send('Ошибка сервера');
+      return;
+    }
+    res.json(JSON.parse(data));
+  });
+});
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Сервер запущен на порту ${PORT}`);
+});
